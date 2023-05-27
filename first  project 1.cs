add50dc22b68bb83cdfe38af904db1f427133058
Atena-Moazen <130185726+Atena-Moazen@users.noreply.github.com>
2023-05-27 07:59:49 +0330

    C#
    کدهای جلسه اول برنامه نویسی مبانی کامپیوتر

---
+++ first  project 1.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace First_prjct
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        string name;
+        Console.WriteLine("plz enter ur name");    
+        name = console.ReadLine();
+        console.WriteLine("hello " + name);
+        console.WriteLine("press a key to exit");
+        console.ReadKey();
+    }
+  }
+}
---
+++ first  project 2.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace First_prjct
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        int num;
+        Console.WriteLine("enter a num");    
+        string number = console.ReadLine();
+        num = int.Parse(number)
+        num *= 3;
+        console.WriteLine(num);
+        console.ReadKey();
+    }
+  }
+}
